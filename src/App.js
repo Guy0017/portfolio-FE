19ff913090199d649f535ed4fefde46c614ff7c9
guy0017 <waesin@gmail.com>
2023-08-31 19:49:02 +0100

    health app with BMI and waist check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-import Header from "./component/header/Header";
-import NavBar from "./component/navBar/NavBar";
-import Profile from "./component/profile/Profile";
-import Contact from "./component/contact/Contact";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './component/header/Header';
+import NavBar from './component/navBar/NavBar';
+import Profile from './component/profile/Profile';
+import Contact from './component/contact/Contact';
+import Projects from './component/projects/Projects';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/profile" element={<Profile />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
